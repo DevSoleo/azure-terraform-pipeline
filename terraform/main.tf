d7666721f545bcb70cac14f221bb8cedb3e160aa
Soleo <devsoleo@protonmail.com>
2022-12-05 17:24:12 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,25 +15,105 @@ provider "azurerm" {
 }
  
 data "azurerm_client_config" "current" {}
- 
-#Create Resource Group
-resource "azurerm_resource_group" "tamops" {
-  name     = "tamops"
-  location = "eastus2"
+
+#Création de mon Ressources groupe
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg"
+  location = "West Europe"
 }
- 
-#Create Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "tamops-vnet"
-  address_space       = ["192.168.0.0/16"]
-  location            = "eastus2"
-  resource_group_name = azurerm_resource_group.tamops.name
+
+#Création de mon réseau
+
+resource "azurerm_virtual_network" "reseauRyan" {
+  name                = "reseauRyan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
 }
- 
-# Create Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.tamops.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "192.168.0.0/24"
+#Création de mon sous réseau
+
+resource "azurerm_subnet" "sous_reseauRyan" {
+    name              ="sous_reseauRyan"
+    resource_group_name = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.reseauRyan.name
+    address_prefixes = ["10.0.2.0/24"]
+}
+#Création de l'adresse ip avec une étiquette DNS
+
+resource "azurerm_public_ip" "mon_ip" {
+    name = "mon_ip"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    allocation_method = "Dynamic"
+    domain_name_label = "dnsryan"
+
+    tags = {
+        environment = "production"
+    }
+}
+
+#Network security groupe and rules
+
+resource "azurerm_network_security_group" "securitegrp" {
+    name = "securitegroupe"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule {
+        name = "SSH"
+        priority = 1001
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "22"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
+}
+
+# Création de l'interface réseau
+
+resource "azurerm_network_interface" "interfacereseau" {
+    name = "myNIC"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+        name = "myNicConfiguration"
+        subnet_id = azurerm_subnet.sous_reseauRyan.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id = azurerm_public_ip.mon_ip.id
+    }
+}
+
+#Connection le groupe de securité à l'interface réseau
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.interfacereseau.id
+  network_security_group_id = azurerm_network_security_group.securitegrp.id
+}
+
+#Création de la machine virutelle
+
+resource "azurerm_linux_virtual_machine" "VmRyan" {
+    name = "Ryan"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    size = "Standard_F2"
+    admin_username = "Ryano"
+    network_interface_ids = [
+        azurerm_network_interface.interfacereseau.id
+    ]
+    source_image_reference {
+       publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "16.04-LTS"
+        version   = "latest"
+    }
+    os_disk {
+      storage_account_type = "Standard_LRS"
+      caching              = "ReadWrite"
+    }
 }
