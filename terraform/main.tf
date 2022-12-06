6b09293b7de2f3b05c7f54ccb8169424e2f08aeb
Soleo <devsoleo@protonmail.com>
2022-12-06 16:51:03 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,6 @@ resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
 
-resource "azurerm_resource_group" "rg" {
-  location = var.resource_group_location
-  name     = random_pet.rg_name.id
-}
-
 # Create virtual network
 resource "azurerm_virtual_network" "my_terraform_network" {
   name                = "myVnet"
