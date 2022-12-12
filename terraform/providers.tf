ce1d9f8bfe34ae63dafe96902851da007269bf6a
Soleo <devsoleo@protonmail.com>
2022-12-12 11:09:30 +0100

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -16,19 +16,3 @@ terraform {
     }
   }
 }
-
-provider "azurerm" {
-
-  subscription_id = "8f7ce552-1171-4141-9803-d3797c089def"
-  client_id       = "2a736400-ca26-4084-bf7c-6d3d1be21519"
-  client_secret   = "JTh8Q~AAAHzVlp3Yb_iTsRFxSA8e6k21mycb5acn"
-  tenant_id       = "0eb69488-ad5b-4e22-a678-ab0b5b9febaa"
-
-  features {}
-}
-
-provider "azuread" {
-  client_id     = "2a736400-ca26-4084-bf7c-6d3d1be21519"
-  client_secret = "JTh8Q~AAAHzVlp3Yb_iTsRFxSA8e6k21mycb5acn"
-  tenant_id     = "0eb69488-ad5b-4e22-a678-ab0b5b9febaa"
-}
