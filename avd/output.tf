2467db2d19c203985d4fcd2919fb9eb2f0fa06fc
Soleo <devsoleo@protonmail.com>
2022-12-19 10:47:44 +0100

    Update output.tf

--- avd/output.tf
+++ avd/output.tf
@@ -25,5 +25,5 @@ output "location" {
 
 output "AVD_user_groupname" {
   description = "Azure Active Directory Group for AVD users"
-  value       = azuread_group.aad_group.display_name
+  value       = ""
 }
